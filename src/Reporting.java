afd4b0ec50416a47ad3e2e662a2846192b2c968c
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-11 11:41:25 +0000

    Added add auction house method

--- src/Reporting.java
+++ src/Reporting.java
@@ -4,6 +4,13 @@ public class Reporting {
 
     public ArrayList<AuctionHouse> auctionHouses = new ArrayList<>();
 
+    public boolean addAuctionHouse(AuctionHouse addedAuctionHouse){
+
+            auctionHouses.add(addedAuctionHouse);
+            return true;
+
+    }
+
     public boolean addItem(Item item) {
         for (AuctionHouse auctionHouse:auctionHouses){
             if(auctionHouse.addItem(item)){
