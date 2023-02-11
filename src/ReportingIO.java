78aac9d55c14f105940fa0ccc88cc2333f0468e3
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-11 15:40:42 +0000

    Added JavaDoc

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -15,39 +15,30 @@ public class ReportingIO {
      * menu() is called which prints out the option the user has which run() gives it functionality.
      * **/
     public void run() {
-
-
-
         boolean quit = false;
         Scanner sc = new Scanner(System.in);
-
         while (!quit) {
-            Scanner input = new Scanner(System.in);
             menu();
             System.out.println("Please select an option");
             int choice = sc.nextInt();
-
-
             switch (choice) {
                 case 1:
                     r.addAuctionHouse(recordAuctionHouse());
-
                     break;
                 case 2:
                     r.addItem(recordItem());
-
                     break;
                 case 3:
-                    sc.nextLine();
-                    System.out.println("Enter auction house name");
-                    String name = sc.nextLine();
-                    r.auctionHouseItems(name);
 
                     break;
                 case 4:
 
                     break;
                 case 5:
+                    sc.nextLine();
+                    System.out.println("Enter auction house name");
+                    String name = sc.nextLine();
+                    r.auctionHouseItems(name);
 
                     break;
                 case 6:
@@ -69,9 +60,15 @@ public class ReportingIO {
                 "2: Add a Item\n" +
                 "3: Filter Items by given price\n" +
                 "4: Most Expensive Item\n" +
-                "5: \n\n");
+                "5: View Items in a specific Auction House");
     }
 
+
+    /**
+     * Code adapted from [source] https://github.com/newcastleuniversity-computing/csc1035-csc1036_practicals_2022/blob/solutions/part1/practical-10/src/IO.java
+     * This method takes user relating to details of the auction house they wish to add
+     * @return AuctionHouse
+     * **/
     private AuctionHouse recordAuctionHouse() {
 
         Scanner sc = new Scanner(System.in);
@@ -86,8 +83,15 @@ public class ReportingIO {
         return new AuctionHouse(name, address, postcode);
     }
 
-    private Item recordItem() {
 
+    /**
+     * Code adapted from [source] https://github.com/newcastleuniversity-computing/csc1035-csc1036_practicals_2022/blob/solutions/part1/practical-10/src/IO.java
+     * This method takes user input relating to the sold Item they wish to add.
+     * It then asks the user which Auction House they want to add the Item to, and uses the searchAuctionouse method to find that Auction House.
+     * It then returns that Item to that Auction House.
+     * @return Item
+     * **/
+    private Item recordItem() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Item name");
         String itemName = sc.nextLine();
@@ -106,17 +110,36 @@ public class ReportingIO {
         System.out.println("Enter name of auction house to add to");
         String auctionHouseName = sc.nextLine();
         AuctionHouse auctionHouse = searchAuctionHouse(auctionHouseName);
-        System.out.println(ANSI_RED + "Items successfully added to " + auctionHouse);
-        return new Item(itemName, itemLotNumber, yearSold, buyer, category, price);
 
+        // if the auctionHouse is not equal to null it will add the Item to the auction house
+        if (auctionHouse != null){
+            //If auctionHouse not equal to null, print statement will run and will return Item
+            System.out.println(ANSI_RED + "Items successfully added to " + auctionHouse);
+            Item item = new Item(itemName, itemLotNumber, yearSold, buyer, category, price);
+            //Adds item to auction house
+            auctionHouse.addItem(item);
+
+        }
+        //If it is equal to null it will return null
+        return null;
     }
 
+    /**
+     * This method searches for an Auction House given the name (not case-sensitive)
+     * @param name
+     * @return Auction house
+     * **/
     private AuctionHouse searchAuctionHouse(String name){
+        //Loops through the auctionHouses Array
         for (AuctionHouse auctionHouse: r.auctionHouses){
+            //If the name inputted matches one in the auction houses array
             if (auctionHouse.getName().equalsIgnoreCase(name)){
+                //It will return the auction house
                 return auctionHouse;
             }
         }
+        //Else the name doesn't match one in the Array it will tell the user it doesn't exist and return null
+        System.out.println(ANSI_RED + "Auction house doesnt exist");
         return null;
 
     }
