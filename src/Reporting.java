12f0f78915b28e5e102020b9f66514acd68da6fa
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-11 23:21:47 +0000

    Added validation for year and length of characters allowed

--- src/Reporting.java
+++ src/Reporting.java
@@ -1,10 +1,15 @@
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reporting {
+
+
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
+    public String userInput;
+    public int convertedYear;
 
 
     //List containing auction houses
@@ -56,17 +61,16 @@ public class Reporting {
      * **/
     public void recordItem() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please Item name");
-        String itemName = sc.nextLine();
+        System.out.println(ANSI_RESET + "Please Item name");
+        String itemName = lengthLimit(userInput, 30);
         System.out.println("Please Enter Item lot number");
         int itemLotNumber = sc.nextInt();
-        System.out.println("Please Enter year sold");
-        int yearSold = sc.nextInt();
+        int yearSold = yearCheck(convertedYear);
         sc.nextLine();
         System.out.println("Please enter buyer name");
-        String buyer = sc.nextLine();
+        String buyer = lengthLimit(userInput, 50);
         System.out.println("Please enter category");
-        String category = sc.nextLine();
+        String category = lengthLimit(userInput, 50);
         System.out.println("Please enter price");
         double price = sc.nextDouble();
         sc.nextLine();
@@ -75,6 +79,7 @@ public class Reporting {
         for (AuctionHouse auctionHouse:auctionHouses){
             if(auctionHouse.getName().equalsIgnoreCase(auctionHouseName)){
                 Item item = new Item(itemName, itemLotNumber, yearSold, buyer, category, price);
+                System.out.println(auctionHouse.getName());
                 System.out.println(ANSI_RED + "Items successfully added to " + auctionHouse + item);
                 //Adds item to auction house
                 auctionHouse.addSoldItem(item);
@@ -98,5 +103,59 @@ public class Reporting {
         }
         return itemsWithGreaterPrice;
     }
+
+
+    /**
+     * This method limits the amount of characters a user can input into a certain field
+     * @param userInput
+     * @param limit
+     * @return userInput
+     * **/
+    private String lengthLimit(String userInput, int limit){
+        Scanner sc = new Scanner(System.in);
+        //do while loop starts
+        do{
+            //Takes user input as a String
+            userInput = sc.nextLine();
+            //Checks condition is met
+            if(userInput.length() > limit){
+                //Prints warning if condition isn't met
+                System.err.println("This cant be longer than " + limit + " characters");
+            }else {
+                //Breaks out of the loop
+                break;
+            }
+        }
+        while (true);
+        return userInput;
+    }
+
+
+    /**
+     * Code adapted from [soruce] https://stackoverflow.com/questions/35936799/validation-so-input-is-only-integer-in-java
+     * This method takes year initially as a String which checks that is a userYear between 0-9 and not larger than 4 characters.
+     * If them conditions are met, it converts the String into an integer which is then checked to make sure it's not less than
+     * the year 2000 and larger than the current year.
+     *
+     * @param userYear - Year the user inputs.
+     * @return converted year input as an integer
+     **/
+    private int yearCheck(int userYear) {
+        Scanner sc = new Scanner(System.in);
+        //Gets the current year
+        Year year = Year.now();
+        int thisYear = year.getValue();
+
+        do {
+            System.out.println("Please enter the year the Item sold between 1980 and " + thisYear);
+            while (!sc.hasNextInt()) {
+                System.err.println("That's not a year. Please enter a year between 1980 and " + thisYear);
+                sc.next(); // this is important!
+            }
+            userYear = sc.nextInt();
+        } while (userYear > thisYear || userYear < 1980);
+        return userYear;
+    }
+
 
 }
