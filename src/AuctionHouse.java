25621a13c282907006d418d2748a2e70b55cf62a
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-11 02:46:47 +0000

    Added AuctionHouse variables, constructor, getter and setter methods

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -1,2 +1,63 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AuctionHouse {
+
+    private String name;
+    private String[] address = new String[2];
+    int numOfItems;
+
+
+    private ArrayList<Item> itemsInAuctionHouse;
+
+
+    /**
+     * Constructor for the Item class
+     * **/
+    AuctionHouse(String name, String firstLineAddress, String postCode){
+        this.name = name;
+        this.address[0] = firstLineAddress;
+        this.address[1] = postCode;
+        this.numOfItems = numOfItems;
+        this.itemsInAuctionHouse = new ArrayList();
+    }
+
+
+    public String[] getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public ArrayList<Item> getItemsInAuctionHouse() {
+
+        return itemsInAuctionHouse;
+    }
+
+    public int amountOfItemsInAuctionHouse(){
+        numOfItems = getItemsInAuctionHouse().size();
+        return numOfItems;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Auction House: \n" +
+                "Name: " + name + "\n" +
+                "Address: " + Arrays.toString(address) + "\n"+
+                "Items in Auction House: " + amountOfItemsInAuctionHouse() + "\n";
+    }
+
+
+
+
 }
