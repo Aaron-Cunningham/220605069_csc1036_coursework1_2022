c257e2ee08d53aca3bee04f08cea98a852968630
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-16 16:53:18 +0000

    Removed unused ANSI variable

--- src/Testing.java
+++ src/Testing.java
@@ -1,6 +1,5 @@
 public class Testing {
     private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
 
     /***
