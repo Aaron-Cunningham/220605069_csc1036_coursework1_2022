6de817b00b74d4210e604117fa86b97a6f2fe024
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-21 12:52:12 +0000

    Fixed filter by greater price method, as it was returning null when items were added

--- src/Reporting.java
+++ src/Reporting.java
@@ -73,12 +73,15 @@ public class Reporting {
         ArrayList<Item> itemsWithGreaterPrice = new ArrayList<>();
         for (AuctionHouse auctionHouse : auctionHouses){
 
-            if(itemsWithGreaterPrice.size() == 0){
-                System.err.println("No items found");
-                return null;
-            }
+
+
+
             //Adds most expensive items to temp list if found to have a greater price than user inputs
             itemsWithGreaterPrice.addAll(auctionHouse.filterItemsByPrice(price));
+        }
+        if(itemsWithGreaterPrice.size() == 0){
+            System.err.println("No items found");
+            return null;
         }
         return itemsWithGreaterPrice;
     }
